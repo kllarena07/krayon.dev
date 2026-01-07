a557660c937e733da1b956c56aa53d69240f5b4e
Kieran Llarena <baymax709@gmail.com>
2026-01-07 15:31:02 -0500

    refactor: switch from actix to axum

--- web/src/main.rs
+++ web/src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{App, HttpResponse, HttpServer, Result, web};
+use axum::{Router, extract::Path, http::StatusCode, response::Html, routing::get};
 use rust_embed::RustEmbed;
 use serde::{Deserialize, Serialize};
 
@@ -15,26 +15,22 @@ struct StaticFiles;
 #[folder = "blog/"]
 struct BlogFiles;
 
-async fn index() -> Result<HttpResponse> {
+async fn index() -> Html<String> {
     let file = StaticFiles::get("index.html").unwrap();
     let content = String::from_utf8(file.data.into_owned()).unwrap();
-    Ok(HttpResponse::Ok().content_type("text/html").body(content))
+    Html(content)
 }
 
-async fn serve_blog_file(path: web::Path<String>) -> Result<HttpResponse> {
-    let filename = path.into_inner();
+async fn blog_file(Path(filename): Path<String>) -> (StatusCode, Html<String>) {
     if let Some(content_file) = BlogFiles::get(&filename) {
-        //let template_file = StaticFiles::get("blog.html").unwrap();
-        //let template = String::from_utf8(template_file.data.into_owned()).unwrap();
         let content = String::from_utf8(content_file.data.into_owned()).unwrap();
-        //let hydrated = template.replace("{{content}}", &content);
-        Ok(HttpResponse::Ok().content_type("text/html").body(content))
+        (StatusCode::OK, Html(content))
     } else {
         not_found().await
     }
 }
 
-async fn serve_blog_index() -> Result<HttpResponse> {
+async fn blog_index() -> Html<String> {
     let template_file = StaticFiles::get("blog.html").unwrap();
     let template = String::from_utf8(template_file.data.into_owned()).unwrap();
 
@@ -51,33 +47,30 @@ async fn serve_blog_index() -> Result<HttpResponse> {
 
     let index_html = template.replace("{{content}}", &links);
 
-    Ok(HttpResponse::Ok()
-        .content_type("text/html")
-        .body(index_html))
+    Html(index_html)
 }
 
-async fn not_found() -> Result<HttpResponse> {
+async fn not_found() -> (StatusCode, Html<String>) {
     let file = StaticFiles::get("404.html").unwrap();
     let content = String::from_utf8(file.data.into_owned()).unwrap();
-    Ok(HttpResponse::NotFound()
-        .content_type("text/html")
-        .body(content))
+    (StatusCode::NOT_FOUND, Html(content))
 }
 
-#[actix_web::main]
+#[tokio::main]
 async fn main() -> std::io::Result<()> {
     println!("Starting server on http://localhost:3000");
 
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(index))
-            .route("/blog", web::get().to(serve_blog_index))
-            .route("/blog/", web::get().to(serve_blog_index))
-            .route("/blog/{filename}", web::get().to(serve_blog_file))
-            .default_service(web::route().to(not_found))
-    })
-    .bind("127.0.0.1:3000")?
-    .run()
-    .await
-}
+    let app = Router::new()
+        .route("/", get(index))
+        .route("/blog", get(blog_index))
+        .route("/blog/", get(blog_index))
+        .route("/blog/{filename}", get(blog_file))
+        .fallback(not_found);
 
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    axum::serve(listener, app).await.unwrap();
+
+    Ok(())
+}
